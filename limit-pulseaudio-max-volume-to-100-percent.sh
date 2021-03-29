d68d7465709818a52aa76ff78d9ea261d1f5804b
Novimatrem <thenovimatrem@protonmail.ch>
2021-03-29 20:45:51 +0100

    v1.0

---
+++ limit-pulseaudio-max-volume-to-100-percent.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+while true
+do 
+
+x=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
+if [[ $x -le 100 ]]
+then
+    echo VOLUME_FINE__SKIP
+fi
+
+if [[ $x -ge 101 ]]
+then
+    echo TOO_LOUD_DETECTED__FIXING
+    amixer -D pulse sset Master 100%
+fi
+
+done
+
+# Limit PulseAudio MAX volume to 100%, to prevent hearing damage/clipping
+
+# initial code snippet (broken) provided by unix stackexchange user Akash Kumar Singh, thanks
+# code fixup and improvement by Novimatrem
+
